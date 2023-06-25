9a736629c6dd0a60f758bd95ccc7466612840145
Manish kumar <mrazz938@gmail.com>
2023-06-25 21:20:05 +0530

    restaurant_details

---
+++ restaurant/src/main/java/com/RestaurantBook/RestaurantList/Model/Restaurant.java
@@ -0,0 +1,18 @@
+package com.RestaurantBook.RestaurantList.Model;
+
+import lombok.Data;
+
+@Data
+public class Restaurant {
+    private Long id= 4567757L;
+    private String name="manish";
+    private String address;
+    private String phoneNumber;
+    private String specialty;
+    private Long totalStaff;
+
+
+    }
+
+
+
